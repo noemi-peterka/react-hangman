0ced05adbc36328a5a5ce06531c69b9f0cebd960
Nomi <naomi.peterka4@gmail.com>
2026-02-17 17:21:57 +0000

    Add some styling tweaks

--- Hangman/src/components/GuessWord.jsx
+++ Hangman/src/components/GuessWord.jsx
@@ -4,8 +4,8 @@ function GuessWord({ word, guessedLetters }) {
     .map((char) => (guessedLetters.includes(char) ? char : "_"))
     .join(" ");
   return (
-    <div className="guess-word">
-      <h2>{display}</h2>
+    <div>
+      <h2 className="guess-word">{display}</h2>
     </div>
   );
 }
